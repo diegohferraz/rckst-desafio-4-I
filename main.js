5d9ab710a9de8566d84fa453ae947dd5b7689d08
Diego Hernandes Ferraz <diegohferraz@hotmail.com>
2024-07-15 11:52:45 -0300

    feat: add code

---
+++ main.js
@@ -0,0 +1,44 @@
+//1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+alert("Hello World!");
+//2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+const num1 = 17;
+const num2 = 13;
+
+console.log(num1 + num2);
+//3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+const num3 = 123;
+
+console.log(typeof num3 === "number" ? "É um número" : "Não é um número");
+//4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+const str1 = "Palavra";
+
+console.log(typeof str1 === "string" ? "É uma string" : "Não é uma string");
+//5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+const bool1 = true;
+
+console.log(typeof bool1 === "boolean" ? "É um booleano" : "Não é um booleano");
+//6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+const num4 = 123;
+const num5 = 456;
+
+console.log(num5 - num4);
+//7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+const num6 = 80;
+const num7 = 12;
+
+console.log(num6 * num7);
+//8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+const num8 = 12;
+const num9 = 4;
+
+console.log(num8 / num9);
+//9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+const num10 = 146;
+
+console.log(num10 % 2 === 0 ? "É um número par" : "Não é um número par");
+
+//10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+const num11 = 245;
+
+console.log(num11 % 2 !== 0 ? "É um número ímpar" : "Não é um número ímpar");
